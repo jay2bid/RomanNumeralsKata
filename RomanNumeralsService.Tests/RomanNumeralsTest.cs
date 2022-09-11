2150f082a34d3592ba4d7a677213b9382a00b4bc
jay2bid <jaybidokwu@gmail.com>
2022-09-11 19:02:34 +0300

    Initial Test

---
+++ RomanNumeralsService.Tests/RomanNumeralsTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RomanNumeralsService.Tests
+{
+	public class RomanNumeralsTest
+	{
+		public RomanNumeralsTest()
+		{
+		}
+	}
+}
+
---
+++ RomanNumeralsService/RomanNumerals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RomanNumeralsService
+{
+	public class RomanNumerals
+	{
+		public RomanNumerals()
+		{
+		}
+	}
+}
+
