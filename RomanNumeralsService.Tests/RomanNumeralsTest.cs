b27675da02f612603e5fde918d6f26233f6e88b5
jay2bid <jaybidokwu@gmail.com>
2022-09-11 20:45:03 +0300

    If applied will convert numbers up to 4999

--- RomanNumeralsService.Tests/RomanNumeralsTest.cs
+++ RomanNumeralsService.Tests/RomanNumeralsTest.cs
@@ -1,11 +1,66 @@
 ﻿using System;
+using FluentAssertions;
 namespace RomanNumeralsService.Tests
 {
 	public class RomanNumeralsTest
 	{
-		public RomanNumeralsTest()
-		{
-		}
-	}
+        private RomanNumerals _RomanNumeralsConverter;
+        [SetUp]
+        public void Setup()
+        {
+            _RomanNumeralsConverter = new RomanNumerals();
+        }
+
+        [Test]
+        public void Roman_Numerals_Returns_I()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(1).Should().Be("I");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_II()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(2).Should().Be("II");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_III()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(3).Should().Be("III");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_IV()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(4).Should().Be("IV");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_CDXLIV()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(444).Should().Be("CDXLIV");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_CMXCIX()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(999).Should().Be("CMXCIX");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_MMCMXCIX()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(2999).Should().Be("MMCMXCIX");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_MMMCMXCIX()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(3999).Should().Be("MMMCMXCIX");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_MMMMCMXCIX()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(4999).Should().Be("MMMMCMXCIX");
+        }
+        [Test]
+        public void Roman_Numerals_Returns_Invalid_Input()
+        {
+            _RomanNumeralsConverter.RomanNumeralsConverter(0).Should().Be("Only values ranging from 1-4999 can be converted using this application");
+        }
+    }
 }
 
--- RomanNumeralsService/RomanNumerals.cs
+++ RomanNumeralsService/RomanNumerals.cs
@@ -6,6 +6,49 @@ namespace RomanNumeralsService
 		public RomanNumerals()
 		{
 		}
+
+			Dictionary<int, string> RomanNumeralsValueKey = new Dictionary<int, string>
+			{
+
+				{1000, "M"},
+				{900, "CM"},
+				{500, "D"},
+				{400, "CD"},
+				{ 100, "C"},
+				{ 90, "XC"},
+                { 40, "XL"},
+                { 10, "X"},
+                { 9, "IX"},
+                { 5, "V"},
+                { 4, "IV"},
+                { 1, "I" }
+			};
+
+			
+        
+		public string RomanNumeralsConverter(int num)
+
+		{
+			string result = "";
+            if (num <= 0 || num > 4999)
+            {
+                result = "Only values ranging from 1-4999 can be converted using this application";
+
+            }
+
+            foreach (var item in RomanNumeralsValueKey)
+			{
+
+				while (num >= item.Key)
+				{
+					result += item.Value;
+					num -= item.Key;
+				}
+			}
+			return result;
+
+
+        } 
 	}
 }
 
